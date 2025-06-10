83132af069a312d2d47006bd9a5485495e1b4514
Hachim E. <azert9412@gmail.com>
2025-06-10 14:28:40 +0100

    hey

--- app/components/Hero/Hero.tsx
+++ app/components/Hero/Hero.tsx
@@ -1,7 +1,6 @@
-
 export default function Hero() {
     return (
-        <section className="w-full h-[80dvh] bg-hero-pattern bg-cover bg-center bg-no-repeat relative" id="Hero">
+        <section className="w-full h-[80dvh] bg-[url('/images/accounting-firm.png')] bg-cover bg-center bg-no-repeat relative" id="Hero">
             <div className="absolute w-full h-full top-0 left-0 hero-fg"></div>
             <div className="container">
                 <div className="flex flex-col justify-center items-start h-[90dvh] w-2/5 max-2xl:w-3/5 max-md:w-5/6 max-xs:w-full space-y-6">
